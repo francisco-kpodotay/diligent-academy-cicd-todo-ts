92841b7dba42f6cf61b5f38a3ab904bcf193c40f
francisco-kpodotay <135797234+francisco-kpodotay@users.noreply.github.com>
2024-10-01 12:28:59 +0200

    remove unnessery imports from app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,3 @@
-import {
-  list,
-  format,
-  add,
-  complete,
-  findById,
-  findByTitle,
-  findByStatus,
-  updateTodo,
-  deleteTodo,
-  addLabel,
-  deleteLabel,
-  findByLabel,
-} from "./todo.js";
-import { display } from "./display.js";
 import { AppError } from "./app-error.js";
 import { TodoStore } from "./interfaces.js";
 import { commands } from "./commands.js";
